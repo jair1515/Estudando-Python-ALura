266f165af52f667e52a644bf1a253c806c01d2b9
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-10 08:05:22 +0000

    Aula 2 modulo 3

--- Est-Alura-Sabor.Exprees/app.py
+++ Est-Alura-Sabor.Exprees/app.py
@@ -7,4 +7,13 @@ print('4. Sair\n')
 
 op = input('Escolha uma opção: ')
 #print("Você escolheu a opção {}".format (op))
-print(f"Você escolheu a opção {op}!")
+#print(f"Você escolheu a opção {op}!")
+
+if op == 1:
+    print('Cadastrar um restaurante')
+elif op == 2:
+    print('Listar restaurantes')
+elif op == 3:
+    print('Ativar restaurantes')
+else:
+    print('Encerrando o programa')    
