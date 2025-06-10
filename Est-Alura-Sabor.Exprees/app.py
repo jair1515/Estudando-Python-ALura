0fc93813e484bd93d0d92ac273e0890dcc27ddcd
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-10 07:14:53 +0000

    Aula 06 Alura python

--- Est-Alura-Sabor.Exprees/app.py
+++ Est-Alura-Sabor.Exprees/app.py
@@ -1,9 +1,10 @@
-print('Sabor Express\n')
+print('𝙎𝙖𝙗𝙤𝙧 𝙀𝙭𝙥𝙧𝙚𝙚𝙨\n')
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print('Você escolheu a opção', opcao_escolhida)
+op = input('Escolha uma opção: ')
+#print("Você escolheu a opção {}".format (op))
+print(f"Você escolheu a opção {op}!")
