bb2b79a5f958c9c86a348f8dc2a37889c03b2e8b
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-10 07:50:28 +0000

    Exercicios Aula 7

---
+++ Est-Alura-Sabor.Exprees/exeAula07.py
@@ -0,0 +1,22 @@
+#print('Python na Escola de Programação da Alura')
+
+#nome = input('Qual seu Nome:')
+#idade = input('Quantos ano voce tem?:')
+
+#print(f'Meu nome é {nome} e tenho {idade} anos!')
+
+#print(' A\n L\n U\n R\n A\n')
+#print('A','L','U','R','A',sep='\n')
+#(""" 
+#A
+#L
+#U
+#R
+#A
+#""")
+#pi = 3.14159
+#print(f'O valor arredondado de pi é: {pi:.2f}')
+
+
+
+      
