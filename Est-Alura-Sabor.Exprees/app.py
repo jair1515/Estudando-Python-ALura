e55d8db51bbef64f71ec8fcefcfc1bcd0409ac07
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-12 05:28:26 +0000

    Aula 07 funções

--- Est-Alura-Sabor.Exprees/app.py
+++ Est-Alura-Sabor.Exprees/app.py
@@ -1,36 +1,37 @@
 import os
 
+def exibir_nome_do_programa():
+    print('𝙎𝙖𝙗𝙤𝙧 𝙀𝙭𝙥𝙧𝙚𝙚𝙨\n')
 
-
-
-print('𝙎𝙖𝙗𝙤𝙧 𝙀𝙭𝙥𝙧𝙚𝙚𝙨\n')
-
-
-
-
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-op = int(input('Escolha uma opção: '))
-#print("Você escolheu a opção {}".format (op))
-#print(f"Você escolheu a opção {op}!")
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
 def finalizar_app():
     os.system('clear')
     print('finalizando o app\n')
 
+def escolher_opcoes():
+    op = int(input('Escolha uma opção: '))
+    #print("Você escolheu a opção {}".format (op))
+    #print(f"Você escolheu a opção {op}!")
+
+    if op == 1:
+        print('Cadastrar um restaurante')
+    elif op == 2:
+        print('Listar restaurantes')
+    elif op == 3:
+        print('Ativar restaurantes')
+    else:
+        finalizar_app() 
+
+def main():   
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcoes()
 
 
-
-
-
-if op == 1:
-    print('Cadastrar um restaurante')
-elif op == 2:
-    print('Listar restaurantes')
-elif op == 3:
-    print('Ativar restaurantes')
-else:
-    finalizar_app()    
+if __name__ == '__main__':
+    main()
