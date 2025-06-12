234ed31228122afa469d823e249b017d3ab5cb61
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-12 04:37:12 +0000

    Aula 4 funções e import

--- Est-Alura-Sabor.Exprees/app.py
+++ Est-Alura-Sabor.Exprees/app.py
@@ -1,13 +1,30 @@
+import os
+
+
+
+
 print('𝙎𝙖𝙗𝙤𝙧 𝙀𝙭𝙥𝙧𝙚𝙚𝙨\n')
+
+
+
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-op = input('Escolha uma opção: ')
+op = int(input('Escolha uma opção: '))
 #print("Você escolheu a opção {}".format (op))
 #print(f"Você escolheu a opção {op}!")
+
+def finalizar_app():
+    os.system('clear')
+    print('finalizando o app\n')
+
+
+
+
+
 
 if op == 1:
     print('Cadastrar um restaurante')
@@ -16,4 +33,4 @@ elif op == 2:
 elif op == 3:
     print('Ativar restaurantes')
 else:
-    print('Encerrando o programa')    
+    finalizar_app()    
