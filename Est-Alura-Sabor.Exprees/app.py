0a25be7e659445ee8cfe0eefc8fbf76a5713ad6f
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-14 05:24:43 +0000

    Hora da pratica modulo 3

--- Est-Alura-Sabor.Exprees/app.py
+++ Est-Alura-Sabor.Exprees/app.py
@@ -1,6 +1,6 @@
 import os
 
-restaurantes = []
+restaurantes = ['Pizza', 'sushi']
 
 
 def exibir_nome_do_programa():
@@ -13,25 +13,35 @@ def exibir_opcoes():
     print('4. Sair\n')
 
 def finalizar_app():
-    os.system('clear')
-    print('finalizando o app\n')
+    exibir_subtitulo('Finalizando o app')
 
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal: ')
+    main()
 
 def opcao_invalida():
     print('Opção invalida!\n')
-    input('Digite uma tecla para voltar ao menu principal: ')
-    main()
+    voltar_ao_menu_principal()
 
+def exibir_subtitulo(texto):
+    os.system('clear')
+    print('texto')
+    print()
 
 def cadastrar_novo_restaurante():
-    os.system('clear')
-    print('Cadastro de novos restaurantes\n')
+    exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('Digite uma tecla para voltar ao menu principal') 
-    main()
+    voltar_ao_menu_principal()
 
+def listar_restaurantes():
+    exibir_subtitulo('Listando os restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
 
 def escolher_opcoes():
     try:
@@ -42,7 +52,7 @@ def escolher_opcoes():
         if op == 1:
             cadastrar_novo_restaurante()
         elif op == 2:
-            print('Listar restaurantes')
+            listar_restaurantes()
         elif op == 3:
             print('Ativar restaurantes')
         elif op == 4:
