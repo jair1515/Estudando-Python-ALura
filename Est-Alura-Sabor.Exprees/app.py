dde9bb74af896cbbfcf402bff51a19ce2a39a11f
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-19 04:14:47 +0000

    MD 05 Aula 03 Docstrings

--- Est-Alura-Sabor.Exprees/app.py
+++ Est-Alura-Sabor.Exprees/app.py
@@ -40,7 +40,16 @@ def exibir_subtitulo(texto):
     print()
 
 def cadastrar_novo_restaurante():
-    '''Essa função e responsavel por cadastrar um novo restaurante'''
+    '''Essa função e responsavel por cadastrar um novo restaurante
+    
+    Inputs:
+    - Nome do restaurantes
+    - Categoria
+
+    Output:
+    - Adiciona um novo restaurante a lista de restaurantes
+    
+    '''
     exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante} já cadastrado: ')
